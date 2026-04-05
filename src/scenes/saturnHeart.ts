a4ffb8752d42247aff7434274f23ae1a514ca7fc
xeone59 <tiankongcaopo@163.com>
2026-04-04 20:59:13 -0700

    Update saturnHeart.ts

--- src/scenes/saturnHeart.ts
+++ src/scenes/saturnHeart.ts
@@ -1,8 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
-const HEART_TEX_RIGHT = '/unlock/zyronmatrix_export_1775333639890.svg'
-const HEART_TEX_LEFT = '/unlock/zyronmatrix_export_1775334780531.svg'
+const HEART_TEX_RIGHT = 'public/unlock/zyronmatrix_export_1775333639890.svgg'
+const HEART_TEX_LEFT = 'public/unlock/zyronmatrix_export_1775334780531.svg'
 
 export type SaturnHeartApi = {
   dispose: () => void
